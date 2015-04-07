4886dc62ecaa497ac0b093e538b4895ddc00cdf4
denesik <denesic@gmail.com>
2015-04-07 09:48:15 +0300

    vao fix

--- Graphic/Render/BaseMesh.cpp
+++ Graphic/Render/BaseMesh.cpp
@@ -6,7 +6,7 @@
 BaseMesh::BaseMesh()
 {
   mVao = 0;
-//  glGenVertexArrays(1, &mVao);
+  glGenVertexArrays(1, &mVao);
   mVideoVertexBuffer = 0;
   mVideoIndexBuffer = 0;
 }
@@ -22,11 +22,11 @@ void BaseMesh::Create()
 
   glGenBuffers(1, &mVideoVertexBuffer);
   glBindBuffer(GL_ARRAY_BUFFER, mVideoVertexBuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(mVertexBuffer[0]) * mVertexBuffer.size(), &mVertexBuffer[0], GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(mVertexBuffer.back()) * mVertexBuffer.size(), &mVertexBuffer[0], GL_STATIC_DRAW);
 
   glGenBuffers(1, &mVideoIndexBuffer);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mVideoIndexBuffer);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(mIndexBuffer[0]) * mIndexBuffer.size(), &mIndexBuffer[0], GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(mIndexBuffer.back()) * mIndexBuffer.size(), &mIndexBuffer[0], GL_STATIC_DRAW);
 
   glBindVertexArray(0);
 }
@@ -50,20 +50,20 @@ void BaseMesh::Use()
 {
   glBindVertexArray(mVao);
 
-  glEnableVertexAttribArrayARB(0);
-  glEnableVertexAttribArrayARB(1);
-  glEnableVertexAttribArrayARB(2);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glEnableVertexAttribArray(2);
 
   GLchar* ptr = 0;
-  glVertexAttribPointerARB(0, 3, GL_FLOAT, GL_FALSE, 32, ptr + sizeof(float) * 0);
-  glVertexAttribPointerARB(1, 2, GL_FLOAT, GL_FALSE, 32, ptr + sizeof(float) * 3);
-  glVertexAttribPointerARB(2, 3, GL_FLOAT, GL_FALSE, 32, ptr + sizeof(float) * 5);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(mVertexBuffer.back()), ptr + sizeof(float) * 0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(mVertexBuffer.back()), ptr + sizeof(float) * 3);
+  glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(mVertexBuffer.back()), ptr + sizeof(float) * 5);
 
   glDrawElements(GL_TRIANGLES, mVideoIndexBuffer, GL_UNSIGNED_INT, NULL);
 
-  glDisableVertexAttribArrayARB(0);
-  glDisableVertexAttribArrayARB(1);
-  glDisableVertexAttribArrayARB(2);
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(2);
 
   glBindVertexArray(0);
 }
